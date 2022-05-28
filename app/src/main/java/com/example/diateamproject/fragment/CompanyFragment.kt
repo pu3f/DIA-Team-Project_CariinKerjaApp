17e0654d92a47d97d7f68ea06e4b8584942e99d0
putri fatimah <putrifatimah428@gmail.com>
2022-05-28 12:24:05 +0700

    fix show link medsos company

--- app/src/main/java/com/example/diateamproject/fragment/CompanyFragment.kt
+++ app/src/main/java/com/example/diateamproject/fragment/CompanyFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.diateamproject.databinding.FragmentCompanyBinding
+import com.example.diateamproject.util.ConvertHtml
 import com.example.diateamproject.util.PrefsLogin
 import com.example.diateamproject.util.PrefsLoginConstant
 import com.example.diateamproject.viewmodel.JobDetailViewModel
@@ -17,6 +18,7 @@ class CompanyFragment : Fragment() {
     private var _binding: FragmentCompanyBinding? = null
     private val binding get() = _binding!!
     private val userId = PrefsLogin.loadInt(PrefsLoginConstant.USERID, 0)
+    val convertHtml = ConvertHtml()
     private val viewModelJobDetail: JobDetailViewModel by lazy {
         ViewModelProviders.of(this).get(JobDetailViewModel::class.java)
     }
@@ -34,19 +36,37 @@ class CompanyFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val jobId = activity?.intent!!.getIntExtra("jobId",0)
-        viewModelJobDetail.getJobById(jobId, userId )
+        val jobId = activity?.intent!!.getIntExtra("jobId", 0)
+        viewModelJobDetail.getJobById(jobId, userId)
         setObserver()
     }
 
     private fun setObserver() {
         viewModelJobDetail.listJobResponse().observe(viewLifecycleOwner, Observer {
+            val ig = it.data.recruiterIg
+            val linkedin = it.data.recruiterLinkedin
+            val fb = it.data.recruiterFb
+
             binding.tvAboutCompany.text = it.data.recruiterDesc
             binding.tvIndustries.text = it.data.recruiterIndustry
-            binding.tvEmployee.text = it.data.recruiterStaff.toString()
-            binding.tvLinkedin.text = it.data.recruiterLinkedin
-            binding.tvFb.text = it.data.recruiterFb
-            binding.tvIg.text = it.data.recruiterIg
+            binding.tvEmployee.text = it.data.recruiterStaff
+            binding.tvWebsite.text = it.data.recruiterWebsite
+            if (linkedin == ("")) {
+                binding.llLinkedin.visibility = View.GONE
+            } else {
+                binding.tvLinkedin.text = linkedin
+            }
+            if (fb == ("")) {
+                binding.llFb.visibility = View.GONE
+            } else {
+                binding.tvFb.text = fb
+            }
+            if (ig == ("")) {
+                binding.llIg.visibility = View.GONE
+            } else {
+                binding.tvIg.text = convertHtml.convertHtmlString(ig)
+            }
+
         })
     }
 }
